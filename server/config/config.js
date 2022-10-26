6db1c0f37aed47fb719ee5030eb765bbc261d2a6
Huang Sheng Wen <shuang81@my.centennialcollege.ca>
2022-10-26 15:46:26 -0400

    Inline Comments Updated.

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,6 @@
 // COMP 229 - Fall 2022 - Web Application Development - Midterm Practical Test
 // Name: Huang Sheng Wen
 // Student ID: 301215574
+//Update to my account and password on MongoDB Atlas
 export const MongoURI = "mongodb+srv://alanhuang81:test123@comp229-f2022-alanhuang.kis4knv.mongodb.net/?retryWrites=true&w=majority";
 export const Secret = "someSecret";
--- server/controllers/books.js
+++ server/controllers/books.js
@@ -19,13 +19,13 @@ export function displayBookList(req, res, next) {
 
 //  GET the Book Details page in order to add a new Book
 export function displayAddPage(req, res, next) {
-
+    //Display the add book page
     res.render('index', {title: 'Add Book', page: 'books/add', book: {} })
 }
 
 // POST process the Book Details page and create a new Book - CREATE
 export function processAddPage(req, res, next) {
-
+    //Receive the user's input
     let newBook = booksModel({
         name: req.body.name,
         author: req.body.author,
@@ -33,7 +33,7 @@ export function processAddPage(req, res, next) {
         description: req.body.description,
         price: req.body.price
     });
-
+    //Create a new book in the database, if there has an error, display it. Otherwise, redirect to the book list page
     booksModel.create(newBook, (err, Book) => {
         if(err){
             console.error(err);
@@ -45,9 +45,9 @@ export function processAddPage(req, res, next) {
 
 // GET the Book Details page in order to edit an existing Book
 export function displayEditPage(req, res, next) {
-
+    //Initialize id is equals to _id(ObjectId), which user can't see it
     let id = req.params.id;
-
+    //Use the particular id to search in the database, if there has an error, display it. Otherwise, display the edit page
     booksModel.findById(id, (err, book) => {
         if(err){
             console.error(err);
@@ -61,9 +61,9 @@ export function displayEditPage(req, res, next) {
 
 // POST - process the information passed from the details form and update the document
 export function processEditPage(req, res, next) {
-
+    //Initialize id is equals to _id(ObjectId), which user can't see it
     let id = req.params.id; 
-
+    //Use the particular id to get the content of the database, such as name, author, published year, description, and price
     let newBook = booksModel({
         _id: req.body.id,
         name: req.body.name,
@@ -72,7 +72,7 @@ export function processEditPage(req, res, next) {
         description: req.body.description,
         price: req.body.price
     });
-
+    //Update the content, if there has an error, display it. Otherwise, redirect to the book list page
     booksModel.updateOne({_id: id}, newBook, (err, Book) => {
         if(err){
             console.error(err);
@@ -84,9 +84,9 @@ export function processEditPage(req, res, next) {
 
 // GET - process the delete by user id
 export function processDelete(req, res, next) {
-
+    //Initialize id is equals to _id(ObjectId), which user can't see it
     let id = req.params.id;
-
+    //Use the particular id to let database know which book is going to remove. If there has an error, display it. Otherwise, redirect to the book list page
     booksModel.remove({_id: id}, (err) => {
         if(err){
             console.error(err);
